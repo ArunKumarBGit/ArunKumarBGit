925dc72b2963db9dc8a7d34944d1551cac3398b4
Arun Kumar B <arunsunlight@yahoo.co.in>
2022-02-02 13:10:04 +0530

    New template file

---
+++ arun_template.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#define SUCCESS             0
+#define printf(msg, ...)    printf("\n"msg"\n", ##__VA_ARGS__)
+
+int
+main (void)
+{
+    int ret_val = SUCCESS;
+
+    return(ret_val);
+}
